3ba08ce95e0d0516a2a55489494d0f1cf2b09fa6
Vincent Stammbach <vincentsta.dev@gmail.com>
2017-08-03 10:11:17 +0200

    boilerplate OK

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "dist/bundle.js"
+  },
+  devtool: "sourcemap"
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  }
+};
